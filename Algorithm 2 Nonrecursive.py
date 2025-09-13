6756d13c2e251a197b5f16f34ddf0af15dc4242b
Carlos Antonio Cruz López <139827225+Cruz-Lopez-Carlos-Antonio@users.noreply.github.com>
2025-09-12 19:45:16 -0600

    Add files via upload

---
+++ Algorithm 2 Nonrecursive.py
@@ -0,0 +1,12 @@
+def Coeff_G(n,vector,a):
+    for m in range(n+1):
+        vector.append([1])
+    for m in range(1,n+1):
+        vector[m].append(sum(a[:m]))
+    for m in range(2,n+1):
+        for k in range(2,m+1):
+            if k==m:
+          vector[m].append(a[0]**m)
+            else:
+                num=a[m-k]*vector[m-1][k-1]
+                vector[m].append(vector[m-1][k]+num)
